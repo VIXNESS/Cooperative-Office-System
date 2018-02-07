d0eff1494032d711ba45f02e45abcdd9f8d9cd38
ViX.neSS <vix2018@hotmail.com>
2018-02-07 13:55:20 +0800

    add project files

---
+++ src/main/java/com/jiangtao/cos/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.jiangtao.cos.controller;
+
+import com.jiangtao.cos.dao.StudentMapper;
+import com.jiangtao.cos.pojo.Student;
+import com.jiangtao.cos.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("stu")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping("get")
+    public Student stuGet(){
+        Student student = studentService.getStudentById(1);
+        return student;
+    }
+
+
+}
---
+++ src/main/java/com/jiangtao/cos/service/StudentService.java
@@ -0,0 +1,7 @@
+package com.jiangtao.cos.service;
+
+import com.jiangtao.cos.pojo.Student;
+
+public interface StudentService {
+    Student getStudentById(int id);
+}
